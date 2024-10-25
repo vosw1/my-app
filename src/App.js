1624fac6e9ff6346965610708211d8238d29eb3e
vosw1 <vosw1@naver.com>
2024-10-25 17:45:35 +0900

    /를 loginForm으로 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginForm from './login/LoginForm';
 
 function App() {
   return (
-    <div className="App">
-      <LoginForm />
-    </div>
+    <Router>
+      <Routes> 
+        <Route path='/' element={<LoginForm />} /> {/* Switch 대신 element 속성 사용 */}
+      </Routes>
+    </Router>
   );
 }
 
